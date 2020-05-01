35da0587940612c59dda8169dccde0f8731a9067
JParrales <josedparrales@gmail.com>
2020-04-30 21:38:26 -0500

    Task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+
+    lenArg = len(sys.argv)
+
+    if lenArg > 1:
+        for i in range(lenArg):
+
+            if i == 0:
+                print("{} arguments:".format(lenArg - 1))
+            else:
+                print("{}: {}".format(i, sys.argv[i]))
+    elif lenArg == 1:
+        print("0 arguments.")
+    else:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))

c9bfed6f02b2c1ce1a50f102843c3e7e5835dde2
JParrales <josedparrales@gmail.com>
2020-05-01 00:17:53 -0500

    Fix4

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,14 +5,9 @@ from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
     argv = sys.argv[1:]
-    chars = ['+', '-', '*', '/']
 
     if len(argv) != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-
-    if argv[1] not in chars:
-        print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
 
     a = int(argv[0])

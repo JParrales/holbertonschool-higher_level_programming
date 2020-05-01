4c1c1feebb356e004649bcac25e21d4a390ef597
JParrales <josedparrales@gmail.com>
2020-05-01 04:01:30 +0000

    chmod +x

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,8 +4,8 @@ from hidden_4 import *
 
 if __name__ == '__main__':
 
-	for i in dir():
-	    if i[0] == i[1] == '_':
-		continue
+    for i in dir():
+        if i[0] == i[1] == '_':
+            continue
 
-	    print(i)	
+        print(i)

242781235887f5af52f3da3f14c069da637d1f0e
JParrales <josedparrales@gmail.com>
2020-09-01 20:29:49 -0500

    ADVANCE

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def remove_char_at(str, n):
+    str2 = str
+
+    if len(str) <= n or n < 0:
+        return str2
+
+    str2 = str2[0: n:] + str2[n + 1]
+
+    return str2

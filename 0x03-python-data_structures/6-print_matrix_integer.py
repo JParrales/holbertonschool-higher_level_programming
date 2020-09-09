bea6548dfafcc5a7500541e6651155225073d013
JParrales <josedparrales@gmail.com>
2020-09-08 23:14:04 -0500

    .format

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,6 +4,6 @@
 def print_matrix_integer(matrix=[[]]):
     for m in range(len(matrix)):
         for n in range(len(matrix[m])):
-            print('{:d} '.format(matrix[m][n]), end = '')
+            print('{:d}'.format(matrix[m][n]), end = ' ')
 
         print()

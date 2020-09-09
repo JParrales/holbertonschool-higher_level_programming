c481eac6de83b45b5195b0a88c34420b693b0181
JParrales <josedparrales@gmail.com>
2020-09-08 22:54:07 -0500

    Task 1 - 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    for m in range(len(matrix)):
+        for n in range(len(matrix[m])):
+            print('{:d} '.format(matrix[m][n]), end = '')
+
+        print()

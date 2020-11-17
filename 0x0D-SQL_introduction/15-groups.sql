739cc12103e08fb8f2f8078110d1e30b48b9ee34
JParrales <josedparrales@gmail.com>
2020-11-16 20:47:44 -0500

    ORDER BY --- DESC

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,6 +1,6 @@
 -- lists the number of records with the same score.
-SELECT score COUNT(score) AS number
+SELECT score, COUNT(score) AS number
 FROM second_table
 GROUP BY score
 HAVING number > 1
-ORDER BY number;
+ORDER BY number DESC;

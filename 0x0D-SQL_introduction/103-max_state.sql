970cfd40f55b234f8db6f4758798c5c343fd5048
JParrales <josedparrales@gmail.com>
2020-11-16 22:11:25 -0500

    name error

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,3 +1,3 @@
 -- displays the max temperature of each state
-SELECT state, MAX(value) 'mac_temp'
-FROM temperatures, GROUP BY state
+SELECT state, MAX(value) 'max_temp'
+FROM temperatures GROUP BY state

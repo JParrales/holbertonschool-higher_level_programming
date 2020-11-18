b0e49846239a96df135181bd33eeef02fb9c75ed
JParrales <josedparrales@gmail.com>
2020-11-17 21:05:03 -0500

    TASK 6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa).
+CREATE DATABASE hbtn_0d_usa;
+USE hbtn_0d_usa
+CREATE TABLE IF NOT EXISTS states (
+     id INT PRIMARY KEY 
+     AUTO_INCREMENT NOT NULL,
+     name VARCHAR(256) NOT NULL 
+ );

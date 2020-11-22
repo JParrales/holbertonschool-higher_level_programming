8703f38a82eaf8894ed9b27c1a806281a2b5f05c
JParrales <josedparrales@gmail.com>
2020-11-22 17:39:48 -0500

    end selector

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -10,3 +10,8 @@ def print_matrix_integer(matrix=[[]]):
                 print('{:d}'.format(matrix[m][n]), end='')
 
         print()
+
+
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+print_matrix_integer(matrix)

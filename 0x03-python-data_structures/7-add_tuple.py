ab31e565af76b524d085197fc86c33bc8aec7ea1
JParrales <josedparrales@gmail.com>
2020-11-23 10:51:56 -0500

    pycodestyle

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,14 +4,14 @@
 def idx_tuple(tuple, idx):
     if len(tuple) <= idx:
         return 0
-    
+
     return tuple[idx]
 
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    
+
     tuple_c = (
-        idx_tuple(tuple_a, 0) + idx_tuple(tuple_b, 0), 
+        idx_tuple(tuple_a, 0) + idx_tuple(tuple_b, 0),
         idx_tuple(tuple_a, 1) + idx_tuple(tuple_b, 1)
     )
 

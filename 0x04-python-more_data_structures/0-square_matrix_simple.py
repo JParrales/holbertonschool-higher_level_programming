91f67de33628f2165c7cd4b81d3f9c9d803faadf
JParrales <josedparrales@gmail.com>
2020-11-26 10:23:05 -0500

    TASK 0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def square_matrix_simple(matrix=[]):
+	
+	new_matrix = []
+
+	for m in range(len(matrix)):
+		new_matrix.append(matrix[m])		
+		for n in range(len(matrix[m])):
+			new_matrix[m][n] = matrix[m][n]**2
+
+	return new_matrix

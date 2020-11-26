3387dac40f4173f813f6bd14808301770f2363df
JParrales <josedparrales@gmail.com>
2020-11-26 18:42:57 -0500

    pycode

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,11 +3,11 @@
 
 def square_matrix_simple(matrix=[]):
 
-	new_matrix = []
+    new_matrix = []
 
-	for m in range(len(matrix)):
-		new_matrix.append(matrix[m].copy())		
-		for n in range(len(matrix[m])):
-			new_matrix[m][n] = matrix[m][n]**2
+    for m in range(len(matrix)):
+        new_matrix.append(matrix[m].copy())
+        for n in range(len(matrix[m])):
+            new_matrix[m][n] = matrix[m][n]**2
 
-	return new_matrix
+    return new_matrix

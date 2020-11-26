8d6feb0684ce4d5e97504ec0e71c160967b93d9f
JParrales <josedparrales@gmail.com>
2020-11-26 18:58:05 -0500

    TASK 1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def search_replace(my_list, search, replace):
+
+    new_list = []
+
+    for i in my_list:
+
+        if i == search:
+            new_list.append(my_list[i])
+        else:
+            new_list.append(my_list[i])
+
+    return new_list

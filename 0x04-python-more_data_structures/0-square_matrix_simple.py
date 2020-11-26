a63b61ebc05c64aca13e0a1d31faaa8c86c4a01f
JParrales <josedparrales@gmail.com>
2020-11-26 11:47:17 -0500

    .copy()

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,13 +1,23 @@
-#!/usr/bin/python3
+""" #!/usr/bin/python3 """
 
 
 def square_matrix_simple(matrix=[]):
-	
+
 	new_matrix = []
 
 	for m in range(len(matrix)):
-		new_matrix.append(matrix[m])		
+		new_matrix.append(matrix[m].copy())		
 		for n in range(len(matrix[m])):
-			new_matrix[m][n] = matrix[m][n]**2
+			new_matrix[m][n] = new_matrix[m][n]**2
 
 	return new_matrix
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+new_matrix = square_matrix_simple(matrix)
+print(new_matrix)
+print(matrix)

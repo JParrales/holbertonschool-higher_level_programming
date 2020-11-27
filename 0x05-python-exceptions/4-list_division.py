c915a86fab81be52453295fc9e50b91ac208e2d0
JParrales <josedparrales@gmail.com>
2020-11-27 06:29:56 -0500

    Task4

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+def list_division(my_list_1, my_list_2, list_length):
+
+    list = []
+
+    for i in range(list_length):
+        try:
+            res = my_list_1[i] / my_list_2[i]
+        except IndexError:
+            print('out of range')
+            res = 0
+        except (ValueError, TypeError):
+            print('wrong type')
+            res = 0
+        except ZeroDivisionError:
+            print('division by 0')
+            res = 0
+        finally:
+            list.append(res)
+
+    return list

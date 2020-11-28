87d8f69918bf0bb5f413314c911fa6d775a7ffc3
JParrales <josedparrales@gmail.com>
2020-11-27 23:37:49 -0500

    fix ecepcion

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,6 +6,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except (ValueError, TypeError) as error:
+    except Exception as error:
         print("Excepion: {}".format(error), file=stderr)
         return False

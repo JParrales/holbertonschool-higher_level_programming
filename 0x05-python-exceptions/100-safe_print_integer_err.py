8454c53c381642e51415979dab5560d0da1b038d
JParrales <josedparrales@gmail.com>
2020-11-27 20:08:16 -0500

    try , excep

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,7 +7,6 @@ def safe_print_integer_err(value):
     try:
         print('{:d}'.format(value))
         return True
-
     except (ValueError, TypeError) as error:
         print('Excepion: {}'.format(error), fiel=stderr)
         return False

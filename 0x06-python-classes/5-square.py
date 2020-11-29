72323a9ff1b28c83834d19a1e2c26032a53d1b7e
JParrales <josedparrales@gmail.com>
2020-11-29 18:19:36 -0500

    TASK 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" Area of a square """
+
+
+class Square:
+    """ class Square that defines a square """
+
+    def __init__(self, size=0):
+        """ Instantiation attribute:size  """
+
+        self.size = size
+
+    def area(self):
+        """ Public instance method: area """
+
+        return self.size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """ Public instance method: my_print """
+
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            print("#" * self.__size)

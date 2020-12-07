bf6e8d902c9c6c639105d843841711bdcccab3dd
JParrales <josedparrales@gmail.com>
2020-12-06 21:56:01 -0500

    PYCODE

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-""" 
+"""
 Task: 0. Integers addition
 Function that adds 2 integers.
 Prototype: def add_integer(a, b=98):
 """
 
+
 def add_integer(a, b=98):
-    """ 
+    """
     Function that adds 2 integers.
     """
     if type(a) is not int and type(a) is not float:

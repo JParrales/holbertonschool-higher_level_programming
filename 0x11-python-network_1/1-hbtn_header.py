7131deee5c7472c18115c3e2ba551f362e824e42
JParrales <josedparrales@gmail.com>
2021-01-11 17:14:04 -0500

    TASK 2

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""script that fetches https://intranet.hbtn.io/status"""
+import urllib.request
+import sys
+
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        print(response.headers['X-Request-Id'])

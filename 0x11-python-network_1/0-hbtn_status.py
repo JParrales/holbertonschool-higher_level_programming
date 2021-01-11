9b38661ceca75e46fee3e2ab62b893b0efeb8eff
JParrales <josedparrales@gmail.com>
2021-01-11 17:03:08 -0500

    pycode

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,7 @@
 import urllib.request
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
 
@@ -12,4 +12,3 @@ if __name__=='__main__':
         print('\t- type: {}'.format(type(html)))
         print('\t- content: {}'.format(html))
         print('\t- utf8 content: {}'.format(html.decode('utf-8')))
-

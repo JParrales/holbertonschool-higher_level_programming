a001284c563b8f544449583d0450996b6b947e60
JParrales <josedparrales@gmail.com>
2021-02-09 12:36:04 -0500

    TASK 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+fs.readFile(process.argv[2], function (err, contents) {
+  if (!err) {
+    console.log(contents.toString().trim());
+  } else {
+    console.log(err);
+  }
+});

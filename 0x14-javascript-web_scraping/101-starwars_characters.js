9552ac0608f8efdabb5216ed854ea3dac5b729c6
JParrales <josedparrales@gmail.com>
2021-02-09 19:00:27 -0500

    PRUEBE 101

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const request = require('request');
+require('request').get('https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/', function (err, r, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    let i = 0;
+    const actors = JSON.parse(body).characters;
+    for (i = 0; i < actors.length; i++) {
+      request(actors[i], function (error, response, body) {
+        if (error) {
+          console.error('error:', error);
+        }
+        console.log(JSON.parse(body).name);
+      });
+    }
+  }
+});

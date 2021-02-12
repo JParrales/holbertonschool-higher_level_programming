fad1647d5bbccee150d7ef970eca8fedc723e79c
JParrales <josedparrales@gmail.com>
2021-02-12 09:06:22 -0500

    Start new version

--- 0x0D-SQL_introduction/103-max_state.sql
+++
@@ -1,3 +0,0 @@
--- displays the max temperature of each state
-SELECT state, MAX(value) 'max_temp'
-FROM temperatures GROUP BY state
--- 0x0D-SQL_introduction/14-average.sql
+++
@@ -1,3 +0,0 @@
--- computes the score average of all records in the table.
-SELECT AVG(score) 'average' 
-FROM second_table;
